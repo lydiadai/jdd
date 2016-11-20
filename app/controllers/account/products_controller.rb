1b187b774f05ba4e957a5a31282048f09b2da19b
lydiadai <lyida0511@gmail.com>
2016-11-20 16:12:08 +0800

    account/product

---
+++ app/controllers/account/products_controller.rb
@@ -0,0 +1,9 @@
+class Account::ProductsController < ApplicationController
+    def index
+        @products = Product.where(is_hidden: false)
+    end
+
+    def show
+        @product = Product.find(params[:id])
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
             end
         end
     end
+    namespace :account do
+        resources :products
+    end
     root 'landingpage#index'
 end
