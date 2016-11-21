9b77506f6e0d5f81380cb23a34994d33e728afa8
lydiadai <lyida0511@gmail.com>
2016-11-21 22:26:57 +0800

    product_list

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,21 @@
+# == Schema Information
+#
+# Table name: orders
+#
+#  id               :integer          not null, primary key
+#  total            :float            default(0.0)
+#  user_id          :integer
+#  billing_name     :string
+#  billing_address  :string
+#  shipping_name    :string
+#  shipping_address :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
 class Order < ApplicationRecord
     belongs_to :user
+    has_many :product_lists
     validates :billing_name, presence: true
     validates :billing_address, presence: true
     validates :shipping_name, presence: true
